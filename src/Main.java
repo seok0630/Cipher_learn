bafb2b4880cb0c55cc72f4b9a88b89d14c1bbdb7
seok0630 <tjrrb0630@gmail.com>
2023-03-20 04:54:43 +0900

    파일 입출력을 통해 사용자의 디렉토리에 다수의 암호를 저장 가능해짐. 디렉토리에 저장된 파일을 읽어들여 사용자가 직접 해독할 문장을 선택가능해짐. 무한반복을 통해 끊김없이 여러번 암호화, 해독이 가능해짐.

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,69 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String args[]) {
+    public static void main (String args[]) throws IOException {
         cipher ci = new cipher("", 0); //암호화를 위한 Cipher 객체 생성
         String pass;
-        int skip;
+        int num, skip;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("1. 암호화      2.복호화");
-        System.out.print("선택: ");
-        switch (scanner.nextInt()) {
-            case 1:
-                System.out.print("암호화할 내용: ");
-                pass = scanner.next();
-
-                System.out.print("암호숫자 선택: ");
-                skip = scanner.nextInt();
-
-                ci = new cipher(pass, skip);
-                ci.encrypt();
-                break;
-            case 2: ci.decrypt();
-                break;
-
-            default:
-                break;
+        File file = new File("./cipher.txt"); //파일 입출력 관련 변수.
+        if (!file.exists()) { //파일 생성. 없을경우 만들지 않음.
+            file.createNewFile();
         }
-        ci.print();
+
+        File file_size = new File("./fsize.txt"); //파일 사이즈 관련 변수.
+        if (!file_size.exists()) { //파일 생성. 없을경우 만들지 않음.
+            file_size.createNewFile();
+
+            FileWriter fsizeWriter = new FileWriter(file_size);
+            fsizeWriter.write("0\n");
+            fsizeWriter.close();
+        }
+
+        for (;;) {
+            System.out.println("----------------------------------");
+            System.out.println("1. 암호화 | 2. 복호화 | 3. 나가기");
+            System.out.println("----------------------------------");
+            System.out.print("선택> ");
+            num = scanner.nextInt();
+
+            if (num == 3) {
+                System.out.println("프로그램이 종료되었습니다.");
+                break;
+            }
+
+            switch (num) {
+                case 1:
+                    printM("암호 초기설정");
+
+                    System.out.print("암호화할 내용: ");
+                    pass = scanner.next();
+
+                    System.out.print("복구키 선택 (0~26): ");
+                    skip = scanner.nextInt();
+
+                    ci = new cipher(pass, skip);
+                    ci.encrypt(file, file_size);
+                    break;
+
+                case 2:
+                    ci.decrypt(file, file_size);
+                    break;
+
+                default:
+                    System.out.println("잘못누르셨습니다. 종료를 원하시면 '3'을 입력해주세요.");
+                    break;
+            }
+            //.print();
+        }
+    }
+
+    static void printM(String say) {
+        System.out.println("----------------------------------");
+        System.out.printf("          %s\n", say);
+        System.out.println("----------------------------------");
     }
 }
 
@@ -42,29 +78,85 @@ class cipher { //암호화를 위한 Cipher 객체 선언
         this.skip = skip;
     }
 
-    void encrypt() {
-        for (int i = 0; i < size; i++)
-            cipher_arr[i] = (char) (cipher_arr[i] + skip);
+    void encrypt(File file, File file_size) throws IOException {
+        FileWriter fileWriter;
+        FileReader fsizeReader = new FileReader(file_size);
+        BufferedReader fbufReader = new BufferedReader(fsizeReader);
 
-        System.out.print("암호화 완료: ");
-    }
+        String say;
+        int number;
 
-    void decrypt() {
-        File f = new File("./cipher.txt");
+        say = fbufReader.readLine();
+        number = Integer.parseInt(say);
 
-        if (f.exists()) {
-            for (int i = 0; i < size; i++)
-                cipher_arr[i] = (char) (cipher_arr[i] - skip);
+        if (number >= 20) {
+            fileWriter = new FileWriter(file);
+            number = 0;
         }
         else {
-            System.out.println("복호화 시킬 파일이 없습니다!");
-            return;
+            fileWriter = new FileWriter(file, true);
+            number = number + 1;
         }
 
-        System.out.print("복호화 완료: ");
+        say = String.valueOf(number);
+
+        FileWriter fsizeWriter = new FileWriter(file_size);
+
+        for (int i = 0; i < size; i++)
+            cipher_arr[i] = (char) (cipher_arr[i] + skip);
+        String str = String.valueOf(cipher_arr);
+
+        fileWriter.write(str + "\r\n");
+        fileWriter.flush();
+        fileWriter.close();
+
+        fsizeWriter.write(say);
+        fsizeWriter.close();
+        fbufReader.close();
+
+        System.out.println("암호화 완료, 복구키를 잊지마세요");
     }
 
-    void print() {
-        System.out.println(cipher_arr);
+    void decrypt(File file, File file_size) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int select,  recovery_key;
+
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufReader = new BufferedReader(fileReader);
+
+        FileReader fsizeReader = new FileReader(file_size);
+        BufferedReader fbufReader = new BufferedReader(fsizeReader);
+
+        String[] line = new String[Integer.parseInt(fbufReader.readLine())];
+
+        String say;
+        int i = 0;
+
+        while ((say = bufReader.readLine()) != null) {
+            line[i] = say;
+
+            i = i + 1;
+        }
+
+        for (int j = 0; j < i; j++) {
+            System.out.printf(j + ". " + line[j] + "\n");
+        }
+
+        System.out.print(("해독할 문장의 번호를 선택해주세요: "));
+        select = scanner.nextInt();
+
+        char[] user_cipher = new char[line[select].length()];
+
+        System.out.print("복구키 입력 (0~26): ");
+        recovery_key = scanner.nextInt();
+
+        for (int k = 0; k < line[select].length(); k++) {
+            user_cipher[k] = (char) (line[select].charAt(k) - recovery_key);
+        }
+
+        System.out.println(user_cipher);
+
+        bufReader.close();
+        fbufReader.close();
     }
 }
